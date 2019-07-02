e2afa84f7ffa736a643eec5035824dea744ad930
Santosh Marigowda <santosharakere@gmail.com>
2019-07-02 20:58:18 +0100

    inserted a document

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const mongoose = require('mongoose');
-// console.log(mongoose);
 
-const db = mongoose.connect('mongodb://localhost/playground', {useNewUrlParser: true})
-.then(() => { 
-    console.log('Connnected to mongodb');
-    console.log('Closing db connection.');
+async function connectToDB() {
+    await mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
+    .then(() => { 
+        console.log('Connnected to mongodb');
+    })
+    .catch(error => console.log(error));
+}
+
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean,
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+const course = new Course({
+    name: 'React JS',
+    author: 'Santosh',
+    tags: ['node', 'backend'],
+    isPublished: true,
+});
+
+// async iffi
+(async function() {
+    await connectToDB();
+    const result = await course.save();
+    console.log(result);
     mongoose.connection.close();
-})
-.catch(error => console.log(error));
+})();
 
