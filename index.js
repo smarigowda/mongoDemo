ce1a6a72c75e42ecb21bdce223f66239a68f7662
Santosh Marigowda <santosharakere@gmail.com>
2019-07-03 06:04:10 +0100

    created some more documents

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 async function connectToDB() {
-    await mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
-    .then(() => { 
-        console.log('Connnected to mongodb');
+  await mongoose
+    .connect("mongodb://localhost/playground", { useNewUrlParser: true })
+    .then(() => {
+      console.log("Connnected to mongodb");
     })
     .catch(error => console.log(error));
 }
 
-
 const courseSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    tags: [ String ],
-    date: { type: Date, default: Date.now },
-    isPublished: Boolean,
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
 });
 
-const Course = mongoose.model('Course', courseSchema);
+const Course = mongoose.model("Course", courseSchema);
 
-const course = new Course({
-    name: 'React JS',
-    author: 'Santosh',
-    tags: ['node', 'backend'],
-    isPublished: true,
-});
+async function createCourse() {
+  const course = new Course({
+    name: "Angular JS",
+    author: "Santosh",
+    tags: ["angular", "js"],
+    isPublished: true
+  });
+  const result = await course.save();
+  console.log(result);
+}
 
 // async iffi
 (async function() {
-    await connectToDB();
-    const result = await course.save();
-    console.log(result);
-    mongoose.connection.close();
+  await connectToDB();
+  await createCourse();
+  mongoose.connection.close();
 })();
-
